30d51dcc94aebc46b97c2c06953b46df05e2670e
Fereshte Nami <nami.fereshte@gmail.com>
2019-05-23 14:53:13 +0200

    Create ilFornoItaliano.php

---
+++ assets/sidor/ilFornoItaliano.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Il Forno Italiano</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div>
+            <h1>Il Forno Italiano</h1>
+            <h3><i>Text om il forno</i></h3>
+        </div>
+
+        <div>
+            <h3><i>Bild på il forno</i></h3>
+            <h3><i>Sen kan vi lägga in APIn här så att kartan visas</i></h3>
+        </div>
+
+        <h2><b>Lämna gärna en kommentar här om vad du tycker om Il Forno Italiano!</b><h2>
+        <form>
+            <br>
+            <input type="text" name="användarNamn" id="användarNamn">
+            <br>
+            <p>Namn:</p>
+            <br>
+
+            <br>
+            <input type="text" name="kommentar" id="kommentar">
+            <br>
+            <p>Kommentar</p>
+            <br>
+
+            <br>
+            <button id="skicka" type="submit" class="skicka">Skicka in</button>
+            <br>
+        </form>
+    </body>
+</html>

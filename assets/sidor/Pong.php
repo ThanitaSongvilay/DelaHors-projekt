8c4fd40fdfa5164abdaffbb56f1841be0931d098
Fereshte Nami <nami.fereshte@gmail.com>
2019-05-23 14:41:40 +0200

    Create Pong.php

---
+++ assets/sidor/Pong.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Pong</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div>
+            <h1>Pong</h1>
+            <h3><i>Text om pong</i></h3>
+        </div>
+
+        <div>
+            <h3><i>Bild på pong</i></h3>
+            <h3><i>Sen kan vi lägga in APIn här så att kartan visas</i></h3>
+        </div>
+
+        <h2><b>Lämna gärna en kommentar här om vad du tycker om Pong!</b><h2>
+        <form>
+            <br>
+            <input type="text" name="användarNamn" id="användarNamn">
+            <br>
+            <p>Namn:</p>
+            <br>
+
+            <br>
+            <input type="text" name="kommentar" id="kommentar">
+            <br>
+            <p>Kommentar</p>
+            <br>
+
+            <br>
+            <button id="skicka" type="submit" class="skicka">Skicka in</button>
+            <br>
+        </form>
+    </body>
+</html>

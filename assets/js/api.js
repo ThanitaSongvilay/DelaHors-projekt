2b34a7e7dc4c9a7031449d33d591ddc30fcde48d
Fereshte Nami <nami.fereshte@gmail.com>
2019-05-24 13:41:10 +0200

    vettefan

--- assets/js/api.js
+++
@@ -1,8 +0,0 @@
-var mapOptions = 
-{
-    center: new google.maps.LatLng(59.85882, 17.63889),
-    zoom: 12,
-    mapTypeId: google.maps.mapTypeId.ROADMAP
-};
-
-new google.maps.Map(document.getElementById('map'), mapOptions);

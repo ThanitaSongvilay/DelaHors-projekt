c9e01acd0c3a8ebff16db2ef5f9357bdc7d683cf
Julia Pettersson <julia.pettersson97@hotmail.com>
2019-05-24 11:51:26 +0200

    NavigationBar

--- assets/sidor/asiatiskt.php
+++ assets/sidor/asiatiskt.php
@@ -3,9 +3,30 @@
 <html>
     <head>
         <title>Asiatiska restauranger</title>
+        <link rel="stylesheet" href="../css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
+        <script type="text/javascript" src="../js/index.js"></script> <!-- koppla till js-->
         <meta charset="UTF-8">
     </head>
     <body>
+      <nav>
+        <p>RestauRank </p>
+        <div class="dropDown">
+          <ul>
+          <li><a href="../../Index.html">Hem</a>
+          </ul>
+          <ul>
+          <li><a href="#">Restauranger</a>
+          <ul>
+          <li><a href="italienskt.php">Italienskt</a>
+          <li><a href="asiatiskt.php">Asiatiskt</a>
+          </ul>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
         <div>
             <h1>Asiatiska restauranger</h1>
             <h3><i>Här kan du hitta ett urval bland asiatiska restauranger</i></h3>

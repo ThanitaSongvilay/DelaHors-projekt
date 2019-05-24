2b3143b8f665b920f59f4d430a673995e2ddb805
Julia Pettersson <julia.pettersson97@hotmail.com>
2019-05-24 10:47:18 +0200

    Ändring
    Vsg

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,55 @@ function kolla()
       return false;
     }
 }
+
+function validateForm()
+{
+  var uname = document.RegisForm.regUname.value;
+  var email = document.RegisForm.regUemail.value;
+  var pword = document.RegisForm.regUpassword.value;
+
+  if((uname.trim() =="" )|| (email.trim() =="") || (pword == ""))
+  {
+
+    alert("Var vänlig och fyll i ett användarnamn, en epost adress och ett lösenord");
+    return false;
+  }
+  else if((validateEmail(email)==true && validatePword(pword)==true))
+  {
+      document.RegisForm.submit();
+      return true;
+  }
+  else
+  {
+   return false;
+   header("Refresh: 0; URL=../Pages/Register.php");
+  }
+}
+
+function validateEmail(email)
+{
+  if(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
+  {
+    return true;
+  }
+
+  else
+  {
+    alert("Ogiltig epost");
+   return false;
+  }
+}
+
+function validatePword(pword)
+{
+  var psword =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+  if(psword.test(pword))
+  {
+    return true;
+  }
+  else
+  {
+   alert("Lösenordet måste bestå av 6-20 bokstäver, en storbokstav och minst en siffra");
+   return false;
+  }
+}

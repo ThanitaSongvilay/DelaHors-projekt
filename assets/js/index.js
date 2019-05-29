1ece1cbf2c01451bfce56edfac9e39a0751eb89a
Julia Pettersson <julia.pettersson97@hotmail.com>
2019-05-29 12:05:52 +0200

    Js för kommentarsfällten

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,9 +32,9 @@ function kollaSök()
 function kollaKommentar()
 {
     var kommentar = document.kommentarForm.kommentar.value;
-    var namn = document.kommentarForm.användarNamn.value;
+    //var namn = document.kommentarForm.användarNamn.value;
 
-      if(kommentar.trim() == "" || namn.trim()=="")
+      if(kommentar.trim() == "")
     {
       alert("Vänligen fyll i kommentarsfälltet");
       return false;

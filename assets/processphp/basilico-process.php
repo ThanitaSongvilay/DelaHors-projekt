41e37474efcd6bc95cbba29b313ec56ae67e3cf9
sandraNma <sandraNma@users.noreply.github.com>
2019-06-02 23:42:04 +0200

    Update basilico-process.php

--- assets/processphp/basilico-process.php
+++ assets/processphp/basilico-process.php
@@ -6,19 +6,38 @@
 	$con=new mysqli($host,$dbusername,$password,$dbname);
 
 	if($con->connect_error)
-	{die("Connection failed: " .$con.connect_error);}
+	{
+		die("Connection failed: " .$con.connect_error);
+	}
 
 	$sql="SELECT Kommentar,Namn FROM Kommentarer WHERE RestId = 5 ";
-	$save=$con->query($sql);
-		while($row=$save->fetch_assoc())
-		{
+
+	if(isset($_SESSION['loggedIn'])&&($_SESSION['loggedIn']==true) && ($_SESSION['Value']==1)){
+		$save=$con->query($sql);
+		while($row=$save->fetch_assoc()){
 		echo'<div class="kommentarer">';
 		echo ' <p> '.$row["Namn"].'</p>';
 		echo ' <p>'.$row["Kommentar"].'</p>';
 		echo "<a href=http://localhost/grupp11_2019/assets/processphp/delete-process.php?KomId=".$row['KomId'].">Delete</a>";
 		echo'</div>';
 		echo"<hr>";
-		}
 	}
+	}
+
+	else if(isset($_SESSION['loggedIn'])&&($_SESSION['loggedIn']==true) && ($_SESSION['Value']==0)){
+		$save=$con->query($sql);
+		while($row=$save->fetch_assoc()){
+		echo'<div class="kommentarer">';
+		echo ' <p> '.$row["Namn"].'</p>';
+		echo ' <p>'.$row["Kommentar"].'</p>';
+		echo'</div>';
+		echo"<hr>";
+	}
+	}
+
+	else{
+		echo"Logga in för att se kommentarer och kommentera!";
+	}
+
 $con->close();
 ?>
